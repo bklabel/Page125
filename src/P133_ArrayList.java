0d1f0c94235c0e7924c63bbd522cc465f46d244c
Kevin <KevinAveryAbel@gmail.com>
2023-08-05 22:15:54 -0400

    P133 a lot of it is working and I learned a lot.

--- src/P133_ArrayList.java
+++ src/P133_ArrayList.java
@@ -27,13 +27,29 @@ public class P133_ArrayList {
 
         int theSize = myList.size();
 
+        System.out.println("size of mylist: " + theSize);
+
         boolean isIn = myList.contains(egg1);
+
+        System.out.println("Is egg1 in myList?: " + isIn);
 
         int idx = myList.indexOf(egg2);
 
         boolean empty = myList.isEmpty();
 
-        myList.remove(egg1);  //cannot resolve method remove(egg1)
+        myList.remove(egg1);
+
+        theSize = myList.size();
+
+        System.out.println("size of mylist: " + theSize);
+
+        int theIndex = myList.indexOf(egg1);
+
+        System.out.println("Index of egg1: " + theIndex);
+
+        theIndex = myList.indexOf(egg2);
+
+        System.out.println("Index of egg2: " + theIndex);
 
     }
 
